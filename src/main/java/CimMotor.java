45d172cd4756b2eed3c67f2a6b797348c49e9e9a
portpiratech <portpiratech@gmail.com>
2020-01-06 20:21:16 -0600

    updated

---
+++ src/main/java/CimMotor.java
@@ -0,0 +1,37 @@
+
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class CimMotor {
+
+		private TalonSRX motor;
+		
+		public CimMotor(int channel) {
+			motor = new TalonSRX(channel);
+			
+		}
+
+		public TalonSRX getMotor() {
+			return motor;
+		}
+
+		public void setMotor(TalonSRX motor) {
+			this.motor = motor;
+		}
+		
+		public void setSpeed(double speed){
+			motor.set(ControlMode.PercentOutput, speed);
+		}
+		
+		public void setSpeed(double speed, double deadband){
+			if(Math.abs(speed) < deadband)
+			{
+				this.setSpeed(0);
+			}
+			else
+			{
+				this.setSpeed(speed);
+			}
+		}
+}
